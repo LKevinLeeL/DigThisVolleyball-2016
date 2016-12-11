0ddf0da898172a12967ece39d82a0ee1c1efd44b
Kevin Lee <LKevinLeeL@gmail.com>
2016-12-11 03:27:26 -0500

    Inplemented instagram api

---
+++ js/instagramAPI.js
@@ -0,0 +1,18 @@
+var mytoken = config.MY_TOKEN,
+    num_photos = 6;
+
+$.ajax({
+	url: 'https://api.instagram.com/v1/users/self/media/recent',
+	dataType: 'jsonp',
+	type: 'GET',
+	data: {access_token: mytoken, count: num_photos},
+	success: function(data){
+ 		console.log(data);
+		for( x in data.data ){
+			$('#ig-feed').append('<div class="col-md-4"><img src="'+data.data[x].images.standard_resolution.url+'"></div>' );
+		}
+	},
+	error: function(data){
+		console.log(data);
+	}
+});
