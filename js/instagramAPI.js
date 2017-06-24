f54c734cb73cd1609dc52d8a5c87ba852a521192
Kevin Lee <LKevinLeeL@gmail.com>
2017-06-23 23:41:32 -0400

    remove console.log

--- js/instagramAPI.js
+++ js/instagramAPI.js
@@ -7,12 +7,10 @@ $.ajax({
 	type: 'GET',
 	data: {access_token: mytoken, count: num_photos},
 	success: function(data){
- 		console.log(data);
 		for( x in data.data ){
 			$('#ig-feed').append('<div class="col-md-4 col-xs-6"><a target="_blank" href="'+ data.data[x].link +'"><img src="'+data.data[x].images.standard_resolution.url+'"></a><div class="likes">' + data.data[x].likes.count + '<i class="fa fa-heart" aria-hidden="true"></i></div><div class="caption hidden-sm hidden-xs">' + data.data[x].caption.text + '</div></div>' );
 		}
 	},
 	error: function(data){
-		console.log(data);
 	}
 });
